1933722bb7b6ff591b4088123bf12f451db0988a
mikeshasaco <mikeshasaco@gmail.com>
2024-07-27 22:20:45 -0500

    manatee

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,10 +44,10 @@
                 <ul class="navbar-nav ml-auto">
                    
                     <li class="nav-item">
-                        <a class="nav-link page-scroll" href="https://twitter.com/unknownbases"> <img src="twitter.png" height="50px" width="50px" alt=""> </a>
+                        <a class="nav-link page-scroll" href="ttps://x.com/MAGASOLANA1"> <img src="twitter.png" height="50px" width="50px" alt=""> </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link page-scroll" href="https://t.me/unknownbases"> <img src="telegram.png" height="50px" width="50px" alt=""> </a>
+                        <a class="nav-link page-scroll" href="https://t.me/+R0FATgdTbUJiNDU5"> <img src="telegram.png" height="50px" width="50px" alt=""> </a>
                     </li>
               
                 </ul>
